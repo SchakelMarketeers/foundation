867a2c83c3a88ebcc82417fe83e788715428e448
Roelof Roos <github@roelof.io>
2016-06-29 10:24:10 +0200

    Removed broken tests

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -40,30 +40,7 @@ if(version_compare(PHP_VERSION, '5.5', '<')) {
 // Load composer
 require_once $ComposerDir;
 
-date_default_timezone_set(App::Timezone);
-
-// Load correct opmode
-if (IN_CLI) {
-    App::setOpMode(App::OPMODE_CLI);
-} else {
-    $userAgent = filter_input(INPUT_SERVER, 'USER_AGENT');
-    $requestUrl = filter_input(INPUT_SERVER, 'REQUEST_URI');
-
-    if(!$userAgent && isset($_SERVER['USER_AGENT']))
-        $userAgent = $_SERVER['USER_AGENT'];
-
-    if(!$requestUrl && isset($_SERVER['REQUEST_URI']))
-        $requestUrl = $_SERVER['REQUEST_URI'];
-
-    // Detect API calls
-    if(preg_match('/^\/api\//', $requestUrl))
-        App::setOpMode(App::OPMODE_API);
-    // Auto-detect Guzzle, since we use it for OAuth2
-    elseif(strpos($userAgent, 'GuzzleHttp/') !== false)
-        App::setOpMode(App::OPMODE_API);
-    else
-        App::setOpMode(App::OPMODE_WEB);
-}
+date_default_timezone_set('Europe/Amsterdam');
 
 // Set cookie param and name
 $cookie = session_get_cookie_params();
